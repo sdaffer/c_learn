89cc45c239d968361b416a7a9e70353604fabe96
Jacob Feltman <jacob.feltman@gmail.com>
2026-01-08 23:10:00 -0700

    safe string copy added

---
+++ junk/junk2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char string[] = "hello world";
+    char string_new[] = "different";
+    printf("%s\n", string);
+    char string[0]
+
+    // use '\0';
+    return 0;
+}
---
+++ string_copy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void safe_strcpy(char *src, char *dest, int dest_size) {
+    if (dest_size <= 0) return;
+    int count = 0;
+    while (*src != '\0' && count < (dest_size - 1)) {
+        *dest = *src;
+        dest++;
+        src++;
+        count++;
+    }
+    *dest = '\0';
+    return;
+}
+
+int main() {
+    char src_var[] = "source string\n";
+    int src_size = sizeof(src_var)/sizeof(char);
+    char dest_var[src_size];
+    printf("source_var = %s\n", src_var);
+    printf("dest_var before = %s\n", dest_var);
+    safe_strcpy(src_var, dest_var, sizeof(dest_var)/sizeof(char));
+    printf("dest_var after = %s\n", dest_var);
+    return 0;
+}
