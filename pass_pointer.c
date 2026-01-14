aab3000e53750498b5b856660ff1f8f09e92620d
Jacob Feltman <jacob.feltman@gmail.com>
2026-01-13 20:03:19 -0700

    pointer updates

--- pass_pointer.c
+++ pass_pointer.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void func(char *str_ptr, int str_length) {
+void func_str_decay(char *str_ptr, int str_length) {
     // from here the char array decays into a pointer that points to the first
     // byte of the array in memory. i can print the memory address with printf()
     // by using the %p formatter. printf() understand this to mean to print
@@ -10,15 +10,16 @@ void func(char *str_ptr, int str_length) {
     // we can print the entire char array by giving the pointer for the first
     // byte of the array in memory to printf using the %s format flag. printf()
     // understands to print the char at each memory address until it finds the
-    // null terminator \0.
+    // null terminator '\0'.
     printf("printf() method:\n");
     printf("str_ptr = %s\n", str_ptr);
 
     // we can also print the string like this where we loop through each memory
     // address making sure to only go as far as one of the function parameters
-    // says
+    // says. notice that we subtract one from the string length because that
+    // includes the null terminator '\0'.
     printf("for loop method:\n");
-    for (int i = 0; i < str_length; i++) {
+    for (int i = 0; i < str_length - 1; i++) {
         printf("%c", *str_ptr);
         str_ptr++;
     }
@@ -28,8 +29,8 @@ void func(char *str_ptr, int str_length) {
     // we loop through each memory address, grab the character, print it, and
     // only do it until we hit the null terminator.
     // we have to reset the pointer to the original memory address though
-    // becuase we've already incremented it from the for loop before this
-    str_ptr -= str_length;
+    // because we've already incremented it from the for loop before this
+    str_ptr -= (str_length - 1);  // note the -1 again. see for loop comment.
     printf("while method:\n");
     while (*str_ptr != '\0') {
         printf("%c", *str_ptr);
@@ -38,9 +39,24 @@ void func(char *str_ptr, int str_length) {
     printf("\n");
 }
 
+void func_int(int *int_ptr) {
+    *int_ptr += 3;
+}
+
 int main() {
     char input[] = "input string";
     int length = sizeof(input)/sizeof(input[0]);
-    func(input, length);
+    func_str_decay(input, length);
+
+    int num = 12;
+    printf("num = %d", num);
+    // we pass in the address of num because the function is
+    // supposed to take a pointer as a parameter
+    func_int(&num);  
+    // we should see that the value of num has now changed
+    printf("num = %d", num);
+    // i think this is how we would do functions with arrays and structs: we
+    // would pass in pointers to those objects
+    
     return 0;
 }
--- pointers.c
+++ pointers.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    int num = 10;
-    int * ptr = &num;
-    printf("%p\n", ptr);
-    printf("%i\n", * ptr);
+    int num = 10; // variable with value
+
+    //& operator means "address of", and * operator is the
+    // indirection or dereference operator. it's both how you define a pointer
+    // varialbe (such as i've done here) and dereference the pointer (pull the
+    // value that lives at the memory address). a pointer is a variable that
+    // stores a memory address as its value. i think it also has it's own
+    // memory address.
+    int *ptr = &num; //pointer variable of type int
+
+    printf("num = %d\n", num);
+    printf("the address of num should be = %p\n", &num);
+    printf("the value of the pointer to this number is = %p\n", ptr);
+    printf("the dereferenced pointer value is = %d\n", *ptr);
+
     return 0;
 }
